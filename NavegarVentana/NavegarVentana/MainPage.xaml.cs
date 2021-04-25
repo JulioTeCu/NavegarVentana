66a0c0f6920bb45fb72712706d07af63b6d1af39
JulioTeCu <julioctehozol@gmail.com>
2021-04-25 16:55:49 -0500

    Agregar archivos de proyecto.

---
+++ NavegarVentana/NavegarVentana/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavegarVentana
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Page1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private async void Page2_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+
+        private async void Page3_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page3());
+        }
+    }
+}
---
+++ NavegarVentana/NavegarVentana/Page2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavegarVentana
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+          if (!string.IsNullOrEmpty(num1.Text) && !string.IsNullOrEmpty(num2.Text))
+            {
+                var uno = double.Parse(num1.Text);
+                var dos = double.Parse(num2.Text);
+                var suma = uno + dos;
+                resultado.Text = suma.ToString();
+            }
+            else
+            {
+                DisplayAlert("Datos Erróneos", "Escribe todos los datos", "Ok");
+            }
+        }
+    }
+}
